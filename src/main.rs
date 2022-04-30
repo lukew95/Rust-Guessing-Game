1b95661890af18e139e6eb23721ace5a08e9e838
Luke Ward <lukeward95@gmail.com>
2022-04-30 12:57:31 +0100

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
+
+fn main() {
+    println!("Guessing Game!");
+
+    let secret_number = rand::thread_rng().gen_range(0..101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop{
+        
+        println!("Please enter your guess.");
+    
+        //Create mutable variable to store user guess
+        let mut guess = String::new();
+        
+        //Get user guess from cmd and store in guess
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+    
+        let guess : u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(..) => continue,
+        };
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }  
+        }
+    }
+}
